effa35f2083758c0f7d848e9ea6670733af81a25
Sadixo <manuelgusella13@gmail.com>
2024-05-08 20:43:50 +0200

    Aggiunta plugin BooleanExpressionComplexity

--- src/main/java/it/unipd/mtss/App.java
+++ src/main/java/it/unipd/mtss/App.java
@@ -1,3 +1,8 @@
+////////////////////////////////////////////////////////////////////
+// [Giuseppe] [Tutino] [2075515]
+// [Manuel] [Gusella] [2076430]
+////////////////////////////////////////////////////////////////////
+
 package it.unipd.mtss;
 
 /**
@@ -9,17 +14,5 @@ public class App
 	public static void main( String[] args )
 	{
 		System.out.println( "Hello World!" );
-		boolean a = true;
-		boolean b = false;
-
-		boolean c = (a & b) | (b ^ a); // OK, 1(&) + 1(|) + 1(^) = 3 (max allowed 3)
-
-		boolean d = (a & b) | (b ^ a) | (a ^ b);
-		// violation above, 'Boolean expression complexity is 5 (max allowed is 3)'
-		// 1(&) + 1(|) + 1(^) + 1(|) + 1(^) = 5
-
-		boolean e = a ^ (a || b) ^ (b || a) & (a | b);
-		// violation above, 'Boolean expression complexity is 6 (max allowed is 3)'
-		// 1(^) + 1(||) + 1(^) + 1(||) + 1(&) + 1(|) = 6
 	}
 }
