356755e8173767d39c2aad9f9b86f516621b13d9
Giuseppe Tutino <giuseppetutino1@gmail.com>
2024-05-12 18:56:41 +0200

    Implementazione conversione dei primi 500 numeri romani

--- src/main/java/it/unipd/mtss/IntegerToRoman.java
+++ src/main/java/it/unipd/mtss/IntegerToRoman.java
@@ -15,6 +15,8 @@ public class IntegerToRoman {
 
     if (number > 0) {
 
+      number = converter(number, 500, "D", romanNumber);
+      number = converter(number, 400, "CD", romanNumber);
       number = converter(number, 100, "C", romanNumber);
       number = converter(number, 90, "XC", romanNumber);
       number = converter(number, 50, "L", romanNumber);
