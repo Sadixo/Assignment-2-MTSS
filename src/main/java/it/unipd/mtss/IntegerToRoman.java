24b34777ad4b5150a7cba402cc18195752b29496
Giuseppe Tutino <giuseppetutino1@gmail.com>
2024-05-12 18:50:10 +0200

    Implementazione conversione dei primi 100 numeri romani

--- src/main/java/it/unipd/mtss/IntegerToRoman.java
+++ src/main/java/it/unipd/mtss/IntegerToRoman.java
@@ -15,6 +15,8 @@ public class IntegerToRoman {
 
     if (number > 0) {
 
+      number = converter(number, 100, "C", romanNumber);
+      number = converter(number, 90, "XC", romanNumber);
       number = converter(number, 50, "L", romanNumber);
       number = converter(number, 40, "XL", romanNumber);
       number = converter(number, 10, "X", romanNumber);
