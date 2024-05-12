79ab597cae0c4b1b52c17dd860df2cf0586fb952
Giuseppe Tutino <giuseppetutino1@gmail.com>
2024-05-12 17:57:30 +0200

    Implementazione conversione dei primi 3 numeri romani

--- src/main/java/it/unipd/mtss/IntegerToRoman.java
+++ src/main/java/it/unipd/mtss/IntegerToRoman.java
@@ -4,9 +4,21 @@
 ////////////////////////////////////////////////////////////////////
 
 package it.unipd.mtss;
+
+import java.lang.StringBuilder;
+
 public class IntegerToRoman {
   public static String convert(int number){
-    // TODO
-    return null;
+    StringBuilder romanNumber = new StringBuilder();
+
+    if (number > 0) {
+      while(number > 0) {
+        romanNumber.append("I");
+        number--;
+      }
+      return romanNumber.toString();
+    }
+
+    return "";
   }
 }
