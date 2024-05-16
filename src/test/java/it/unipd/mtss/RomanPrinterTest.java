9f4fadc6fdbc53583e5f979cdb0ed5e199c01da8
Sadixo <manuelgusella13@gmail.com>
2024-05-16 09:41:18 +0200

    Creazione test stampa AsciiArt dei primi 6 numeri

--- src/test/java/it/unipd/mtss/RomanPrinterTest.java
+++ src/test/java/it/unipd/mtss/RomanPrinterTest.java
@@ -15,43 +15,58 @@ import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 public class RomanPrinterTest {
 
-  private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-  private final PrintStream originalOut = System.out;
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
 
-  @BeforeEach
-  public void setUpStreams() {
-    System.setOut(new PrintStream(outContent));
-  }
+    @BeforeEach
+    public void setUpStreams() {
+	System.setOut(new PrintStream(outContent));
+    }
 
-  @AfterEach
-  public void restoreStreams() {
-    System.setOut(originalOut);
-  }
+    @AfterEach
+    public void restoreStreams() {
+	System.setOut(originalOut);
+    }
 
-  @ParameterizedTest
-  @MethodSource("stringToAsciiArt")
-  void checkAsciiArt(int romanNum, String asciiNumber) {
-    RomanPrinter.print(romanNum);
-    assertEquals(asciiNumber, outContent.toString());
-  }
+    @ParameterizedTest
+    @MethodSource("stringToAsciiArt")
+    void checkAsciiArt(int romanNum, String asciiNumber) {
+	RomanPrinter.print(romanNum);
+	assertEquals(asciiNumber, outContent.toString());
+    }
 
-  private static Stream<Arguments> stringToAsciiArt() {
-    return Stream.of(
-        arguments(1,"  ___ \n"+
-                  " |_ _|\n"+
-                  "  | | \n"+
-                  "  | | \n"+
-                  " |___|\n"),
-          arguments(2,"  ___   ___ \n"+
-                  " |_ _| |_ _|\n"+
-                  "  | |   | | \n"+
-                  "  | |   | | \n"+
-                  " |___| |___|\n"),
-          arguments(3,"  ___   ___   ___ \n"+
-                  " |_ _| |_ _| |_ _|\n"+
-                  "  | |   | |   | | \n"+
-                  "  | |   | |   | | \n"+
-                  " |___| |___| |___|\n")
-        );
-  } 
+    private static Stream<Arguments> stringToAsciiArt() {
+	return Stream.of(
+			 arguments(1," ___ \n"+
+				   "|_ _|\n"+
+				   " | | \n"+
+				   " | | \n"+
+				   "|___|\n"),
+			 arguments(2," ___  ___ \n"+
+				   "|_ _||_ _|\n"+
+				   " | |  | | \n"+
+				   " | |  | | \n"+
+				   "|___||___|\n"),
+			 arguments(3," ___  ___  ___ \n"+
+				   "|_ _||_ _||_ _|\n"+
+				   " | |  | |  | | \n"+
+				   " | |  | |  | | \n"+
+				   "|___||___||___|\n"),
+			 arguments(4," ___ __     __\n"+
+				   "|_ _|\\ \\   / /\n"+
+				   " | |  \\ \\ / / \n"+
+				   " | |   \\ V /  \n"+
+				   "|___|   \\_/   \n"),
+			 arguments(5,"__     __\n"+
+				   "\\ \\   / /\n"+
+				   " \\ \\ / / \n"+
+				   "  \\ V /  \n"+
+				   "   \\_/   \n"),
+			 arguments(6,"__     __ ___ \n"+
+				   "\\ \\   / /|_ _|\n"+
+				   " \\ \\ / /  | | \n"+
+				   "  \\ V /   | | \n"+
+				   "   \\_/   |___|\n")
+			 );
+    } 
 }
